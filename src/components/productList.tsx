472ef1b30bb2c3793d97257d924254e4ff4f1a73
NRLacerda <nrochalacerda@gmail.com>
2022-11-09 16:55:20 -0300

    db fetch

--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -5,7 +5,10 @@ import * as React from 'react';
         const products = ["BK1", "BK2", "BK3", "BK4", "BK4"];
         const values = [1234,1234,1234,1234,1234]
         // Depois fazer
-      
+        fetch('https://api.github.com/users/manishmshiva', {
+            method: "GET",
+            headers: {"Content-type": "application/json;charset=UTF-8"}
+          })
         return (
           <ul>
             { products.map( (product) => <li>{ product }</li>) }
