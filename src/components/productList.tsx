e3e6716b3c9c3795878e4539be5ff546a0be6921
NRLacerda <nrochalacerda@gmail.com>
2022-11-09 17:32:11 -0300

    att

--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -1,19 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
-
-    const ProductList = () => {
-        const products = ["BK1", "BK2", "BK3", "BK4", "BK4"];
-        const values = [1234,1234,1234,1234,1234]
-        // Depois fazer
-        fetch('https://api.github.com/users/manishmshiva', {
-            method: "GET",
-            headers: {"Content-type": "application/json;charset=UTF-8"}
-          })
-        return (
-          <ul>
-            { products.map( (product) => <li>{ product }</li>) }
-            { values.map( (value) => <li>{ value }</li>) }
-          </ul>
-        );
-      }
-      export default ProductList;
+const ProductList = () => {
+	interface produto {
+		nome: string;
+		valor: number;
+		valorVista: number;
+	}
+	const s = ["BK1", "BK2", "BK3", "BK4", "BK4"];
+	const values = [1234, 1234, 1234, 1234, 1234];
+	// Depois fazer
+	const products = fetch(`https://urldaAPI.com`)
+		.then((response) => response.json())
+		.then((data) => console.log(data.name));
+	return (
+		<ul>
+			{products.map((product) => (
+				<li>{product}</li>
+			))}
+			{values.map((value) => (
+				<li>{value}</li>
+			))}
+		</ul>
+	);
+};
+export default ProductList;
